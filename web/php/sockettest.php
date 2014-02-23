7d518d1f58bec53d524157debd3bb74aa2206799
c3ma <c3ma@calculator>
2014-02-23 20:35:17 +0100

    sh binary interface

--- web/php/sockettest.php
+++ web/php/sockettest.php
@@ -1,8 +1,63 @@
 <?php
 
+/**
+ * Abstract class which serves as an enum to allow nice switch-case
+ * statements or decisions like if($foo == LightType::RGB)
+ */
+abstract class LightType{
+	const RGB = "C3MARGB";
+	const BINARY = "C3MABinary";
+}
+
+
+abstract class Status{
+	const ENABLED = "h";
+	const DISABLED = "l";
+}
+
 class C3MALight{
 
-	private 
+	/**
+	 * @var holds the PATH to the configuration
+	 */
+	private static $config = "config.ini";
+
+	/**
+	 * @var holds the actual configuration after construction
+ 	 */
+	private $configuration;
+
+	public function __construct(){
+		$this->configuration = parse_ini_file(self::$config,TRUE);
+	}
+
+	/**
+	 * Returns a socket connection to a specific type of lightning
+	 * infrastrcuture.
+	 *
+	 * @param $type LightType object 
+	 * @return Socket or throw exception
+	 */
+	private function getConnection($type) {
+		$section = $this->configuration[$type];
+		$fp = fsockopen($section["host"], $section["port"], $errno, $errstr, 3);
+		if(!$fp){
+			throw new Exception($errno." : ".$errstr);
+		}else{
+			return $fp;
+		}
+	}
+
+
+	/**
+	 * Closes a socket passed as parameter
+	 * 
+	 * @param $fp Socket
+	 */
+	private function closeConnection($fp) {
+		fclose($fp);
+	}
+
 /**
  * for binary switched lights the following commands apply:
  * ollpera | read actual status of all lamps reponse looks like:
@@ -15,6 +70,29 @@ class C3MALight{
  * ollpew<n><h|l> | write status <l> (low, off) or <h> (high, on) 
  * 		  | to lamp with id <n>. Response same as ollpera
  */
+	public function getBinaryAll(){
+		$connection = $this->getConnection(LightType::BINARY);
+		fwrite($connection, "ollpera\n");
+		$response = "";
+		do{
+			$response .= fgets($connection);
+		}while(!strstr($response,"ACK"));
+		$this->closeConnection($connection);
+		$result = explode(' ',strstr(str_replace(array("\n"), ' ',$response),"states"));
+		return $result[1];
+	}
+
+	public function getBinary($number){
+		$result = $this->getBinaryAll();
+		return $result{$number-1};
+	}
+
+	public function setBinary($number, $status) {
+		$connection = $this->getConnection(LightType::BINARY);
+		fwrite($connection, "ollpew".$number.$status."\n");
+		$this->closeConnection($connection);
+	}
+
 
 
 /**
@@ -29,16 +107,11 @@ class C3MALight{
  * dmx show | returns actual status of all values (channels) as HEX
  * values. 
  */
-$fp = fsockopen("10.23.42.140", 2001, $errno, $errstr, 3);
-if (!$fp) {
-    echo "$errstr ($errno)<br />\n";
-} else {
-    fwrite($fp, "ollpew1h\n");
-    while (!feof($fp)) {
-        echo fgets($fp, 2048);
-    }
-    fclose($fp);
-}
-
 
 }
+
+$c3ma = new C3MALight();
+var_dump($c3ma->getBinary(1));
+
+
+$c3ma->setBinary(1, Status::ENABLED);
