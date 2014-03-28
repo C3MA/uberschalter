3993a72758826339b77f31e3495cea665cfac32f
tobi <mail@tobima.de>
2014-03-29 00:13:35 +0100

    fixed wrong return value of binary api function in client, too

--- web/www/scripts/uberschalter.js
+++ web/www/scripts/uberschalter.js
@@ -1,6 +1,6 @@
 function updateSwitch( key, val ) {
   console.log("Key = " + key + ":" + val);
-  $('#'+key).prop('checked', (val > 0) ? true : false).trigger('create').flipswitch('refresh');
+  $('#'+key).prop('checked', (val > 'h') ? true : false).trigger('create').flipswitch('refresh');
 };
 
 function getBStatus(key, callback) {
