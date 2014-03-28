6e97de6c01b42ab507686faf8fe157c2db4d4f8f
tobi <mail@tobima.de>
2014-03-28 22:02:46 +0100

    move js code to seperate file

---
+++ web/www/scripts/uberschalter.js
@@ -0,0 +1,28 @@
+
+function getAllStates() {
+    for (var i=1;i<=6;i++){
+        $.get( "php/api.php", { type : "bin" , id : i }).done( function( data ) {
+            $.each(jQuery.parseJSON(data), function( key, val ) {
+	        console.log("Key = " + key + ":" + val);
+	        $('#'+key).prop('checked', (val == "h") ? true : false).trigger('create').checkboxradio('refresh');
+            });
+        });
+    } 
+};
+
+/* main */
+$({
+  $(':checkbox').change(function() {
+        $.get( "php/api.php", { type : "bin" , id : this.name, v: (this.checked) ? 1 : 0 }).done( function( data ) {
+                $.each(jQuery.parseJSON(data), function( key, val ) {
+                console.log("Key = " + key + ":" + val);
+                $('#'+key).prop('checked', (val == "h") ? true : false).trigger('create').checkboxradio('refresh');
+                });
+        });
+  });
+
+  getAllStates();
+
+  window.setInterval(getAllStates, 2000);
+
+});
