c6a3b6b28af6c817fa09f98204f0a4f005a0f6bb
tobi <mail@tobima.de>
2014-03-28 22:54:59 +0100

    reorg scripts

--- web/www/scripts/uberschalter.js
+++ web/www/scripts/uberschalter.js
@@ -1,24 +1,34 @@
 
 function getAllStates() {
     for (var i=1;i<=6;i++){
-        $.get( "php/api.php", { type : "bin" , id : i }).done( function( data ) {
-            $.each(jQuery.parseJSON(data), function( key, val ) {
-	        console.log("Key = " + key + ":" + val);
-	        $('#'+key).prop('checked', (val == "h") ? true : false).trigger('create').checkboxradio('refresh');
-            });
-        });
+        $.get( "php/api.php", 
+	      { 
+		 type : "bin" ,
+	         id : i 
+	      }).done( function( data ) {
+                  $.each(jQuery.parseJSON(data), function( key, val ) {
+	              console.log("Key = " + key + ":" + val);
+	              $('#'+key).prop('checked', (val == "h") ? true : false).trigger('create').checkboxradio('refresh');
+                  });
+              });
     } 
 };
 
 /* main */
 $({
   $(':checkbox').change(function() {
-        $.get( "php/api.php", { type : "bin" , id : this.name, v: (this.checked) ? 1 : 0 }).done( function( data ) {
-                $.each(jQuery.parseJSON(data), function( key, val ) {
-                console.log("Key = " + key + ":" + val);
-                $('#'+key).prop('checked', (val == "h") ? true : false).trigger('create').checkboxradio('refresh');
-                });
-        });
+        $.get( "php/api.php",
+	      { 
+	          type : "bin" ,
+	          id : this.name, 
+	          v: (this.checked) ? 1 : 0 
+		 
+	      }).done( function( data ) {
+                  $.each(jQuery.parseJSON(data), function( key, val ) {
+                      console.log("Key = " + key + ":" + val);
+                      $('#'+key).prop('checked', (val == "h") ? true : false).trigger('create').checkboxradio('refresh');
+                  });
+              });
   });
 
   getAllStates();
